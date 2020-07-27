7cb8c4271e250fb13ea1f497ce43f866ad6503cc
Rich Pauloo <richpauloo@gmail.com>
2020-07-27 07:38:42 -0700

    calculate failure count per gsa/scenario in model

--- code/04_model.R
+++ code/04_model.R
@@ -34,23 +34,23 @@ run_model <- function(d, selected_gsp, decline){
   # vector of declines
   decline <- decline
 
-  for(i in 1:length(decline)){
+  for(j in 1:length(decline)){
     
     # high estimate of failures (lower CI of pump location)
     result_pump_loc <- 
-      ifelse(d@data$mean_ci_lower >= (d@data$gwl_2019 + decline[i]), 
+      ifelse(d@data$mean_ci_lower >= (d@data$gwl_2019 + decline[j]), 
              "active","failing"
              ) %>% 
-      as_tibble() %>% 
-      setNames(paste0("high_", decline[i])) 
+      as.data.frame() %>% 
+      setNames(paste0("high_", decline[j])) 
     
     # low estimate of well failure (total completed depth + min suction head)
     result_tot_dpth <- 
-      ifelse(d@data$tot_depth_msh >= (d@data$gwl_2019 + decline[i]), 
+      ifelse(d@data$tot_depth_msh >= (d@data$gwl_2019 + decline[j]), 
              "active","failing"
              ) %>% 
-      as_tibble() %>% 
-      setNames(paste0("low_", decline[i]))
+      as.data.frame() %>% 
+      setNames(paste0("low_", decline[j]))
     
     # bind to d
     d@data <- cbind.data.frame(d@data, result_pump_loc, result_tot_dpth)
@@ -59,21 +59,38 @@ run_model <- function(d, selected_gsp, decline){
     #lapply(list(result_pump_loc, result_tot_dpth), table)
   }
   
+  # compute fail count per decline scenario
+  failn <- dplyr::select(d@data, matches("low_|high_")) %>% 
+    apply(2, function(x) length(x[x == "failing"]) )
+  
   # compute fail percentages per decline scenario
   failp <- dplyr::select(d@data, matches("low_|high_")) %>% 
     apply(2, function(x) length(x[x == "failing"])/nrow(d)) 
   
   # create errorbar dataframe for ggplot/plotly
   edf <- tibble(decline    = decline, 
+                failn_high = failn[seq(1,length(failn),2)],
+                failn_low  = failn[seq(2,length(failn),2)],
                 failp_high = failp[seq(1,length(failp),2)],
                 failp_low  = failp[seq(2,length(failp),2)]) %>% 
-    mutate(cost_low  = failp_low  * nrow(d) * cost_per_failed_well,
-           cost_high = failp_high * nrow(d) * cost_per_failed_well) %>%
+    mutate(cost_low  = failn_low  * cost_per_failed_well,
+           cost_high = failn_high * cost_per_failed_well) %>%
     mutate(failp_mean = (failp_high + failp_low)/2) %>% 
-    mutate(text = paste0("<em>groundwater decline:</em> ", decline, " ft<br>",
-                         "<em>failure rate:</em> ", round(100 * failp_low, 1), " - ", 
-                         round(100 * failp_high, 1), " %", "<br>",
-                         "<em>estimated cost:</em> $", round(cost_low, 1), " - ", round(cost_high, 1), "M"))
+    mutate(text = paste0("<em>groundwater decline:</em> ", 
+                         decline, " ft<br>",
+                         
+                         "<em>failure count:</em> ", 
+                         failn_low, " - ", 
+                         failn_high, "br>",
+                         
+                         "<em>failure rate:</em> ", 
+                         round(100 * failp_low, 1), " - ", 
+                         round(100 * failp_high, 1), " %<br>",
+                         
+                         "<em>estimated cost:</em> $", 
+                         round(cost_low, 1), " - ", 
+                         round(cost_high, 1), "M")
+    )
   
   return(list(failp = edf, sp = d))
 }
