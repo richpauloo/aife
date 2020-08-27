16d6064dbbe90fcb42b34e66d4f5c22be6166c2d
Rich Pauloo <richpauloo@gmail.com>
2020-08-27 16:14:55 -0700

    reorder and make plotly MTs reflect MT nfail ranges

--- code/07_postprocess.R
+++ code/07_postprocess.R
@@ -73,75 +73,104 @@ for(i in 1:length(decline_v)){
 # ------------------------------------------------------------------------
 # plotly popups of all MT CIs or range per GSA
 # ------------------------------------------------------------------------
-# bootstrapped and range output from `01_sampling_distribution...R`
-bs  <- read_rds(here("code", "results", "MT_diffs_bootstrapped.rds"))
-rg  <- read_rds(here("code", "results", "MT_diffs_range.rds"))
-key <- read_csv(here("data", "gsa_key.csv")) %>% 
-  filter(!is.na(gsp_name))
+# # bootstrapped and range output from `01_sampling_distribution...R`
+# bs  <- read_rds(here("code", "results", "MT_diffs_bootstrapped.rds"))
+# rg  <- read_rds(here("code", "results", "MT_diffs_range.rds"))
+# key <- read_csv(here("data", "gsa_key.csv")) %>% 
+#   filter(!is.na(gsp_name))
+# 
+# # sanity check is FALSE
+# (length(bs) + length(rg)) == (length(gsa_names) - 1)
+# 
+# gsa_names_model_order <- 
+#   c(sapply(bs, function(x) x$name[1]),
+#     sapply(rg, function(x) x$name[1])) %>% 
+#     tolower() %>% 
+#     str_remove_all("[\\(\\)]") %>% 
+#     str_remove_all(" dm") %>% 
+#     str_replace_all(" |-", "_") 
+# 
+# # extract IRR of bootstrapped output and range of range output
+# x1 <- map(bs, "z") %>% 
+#   map(quantile, c(0.25, 0.75))  %>% 
+#   map_df(bind_rows) %>% 
+#   rename(q1 = `25%`, q2 = `75%`) %>% 
+#   mutate(type = "IQR")
+# x2 <- bind_rows(rg) %>% 
+#   rename(q1 = z1, q2 = z2) %>% 
+#   select(q1, q2) %>% 
+#   mutate(type = "range")
+# n <- c(map(bs, "n") %>% map(1) %>% unlist(),
+#        map(rg, "n") %>% unlist())
+# 
+# # construct gwl decline MT intervals and get the gsp names right!
+# ivs <- bind_rows(x1, x2) %>% 
+#   mutate(n = n,
+#          name = gsa_names_model_order) %>% 
+#   # somehow olcese sneaked in... remove it
+#   filter(name %in% key$gsp_name_model) %>% 
+#   left_join(key, by = c("name" = "gsp_name_model")) %>% 
+#   select(-name) %>% 
+#   left_join(tibble(name = gsa_names[-1], 
+#                    full_name = names(l)[-1]),
+#             by = c("gsp_name" = "name")) %>% 
+#   mutate(low  = round(q1, -1), 
+#          high = round(q2, -1))
+# 
+# # sanity checks pass!
+# ivs$gsp_name  %in% gsa_names
+# ivs$full_name %in% names(l)[-1]
+# 
+# # add ALL
+# z   <- map(bs, "z") %>% unlist() %>% quantile(c(0.25,0.75))
+# ncv <- map(bs, "n") %>% map(1) %>% unlist() %>% sum()
+# ivs <- bind_rows(
+#   tibble(q1=z[1], q2=z[2],type="IQR",n=ncv,
+#          gsp_name="ALL",full_name="ALL",
+#          low=round(z[1],-1), high=round(z[2],-1)),
+#   ivs)
 
-# sanity check is FALSE
-(length(bs) + length(rg)) == (length(gsa_names) - 1)
+# read MT summary for all GSAs and ALL that replaces IVs above
+mt_fail_summary <- read_rds(here("code", "results", "mt_fail_summary.rds"))
+mt_fail_summary <- mt_fail_summary %>% 
+  left_join(tibble(name = gsa_names, 
+                   full_name = names(l)),
+            by = c("gsp_name" = "full_name")) %>% 
+  rename(full_name = gsp_name,
+         gsp_name  = name) %>% 
+  select(gsp_name, everything())
 
-gsa_names_model_order <- 
-  c(sapply(bs, function(x) x$name[1]),
-    sapply(rg, function(x) x$name[1])) %>% 
-    tolower() %>% 
-    str_remove_all("[\\(\\)]") %>% 
-    str_remove_all(" dm") %>% 
-    str_replace_all(" |-", "_") 
-
-# extract IRR of bootstrapped output and range of range output
-x1 <- map(bs, "z") %>% 
-  map(quantile, c(0.25, 0.75))  %>% 
-  map_df(bind_rows) %>% 
-  rename(q1 = `25%`, q2 = `75%`) %>% 
-  mutate(type = "IQR")
-x2 <- bind_rows(rg) %>% 
-  rename(q1 = z1, q2 = z2) %>% 
-  select(q1, q2) %>% 
-  mutate(type = "range")
-n <- c(map(bs, "n") %>% map(1) %>% unlist(),
-       map(rg, "n") %>% unlist())
-
-# construct gwl decline MT intervals and get the gsp names right!
-ivs <- bind_rows(x1, x2) %>% 
-  mutate(n = n,
-         name = gsa_names_model_order) %>% 
-  # somehow olcese sneaked in... remove it
-  filter(name %in% key$gsp_name_model) %>% 
-  left_join(key, by = c("name" = "gsp_name_model")) %>% 
-  select(-name) %>% 
-  left_join(tibble(name = gsa_names[-1], 
-                   full_name = names(l)[-1]),
-            by = c("gsp_name" = "name")) %>% 
-  mutate(low  = round(q1, -1), 
-         high = round(q2, -1))
-
-# sanity checks pass!
-ivs$gsp_name  %in% gsa_names
-ivs$full_name %in% names(l)[-1]
-
-# add ALL
-z   <- map(bs, "z") %>% unlist() %>% quantile(c(0.25,0.75))
-ncv <- map(bs, "n") %>% map(1) %>% unlist() %>% sum()
-ivs <- bind_rows(
-  tibble(q1=z[1], q2=z[2],type="IQR",n=ncv,
-         gsp_name="ALL",full_name="ALL",
-         low=round(z[1],-1), high=round(z[2],-1)),
-  ivs)
-
+ivs <- vector("list", length = length(nrow(mt_fail_summary)))
 # write the MT decline ggplots and plotly objects
-for(j in 1:nrow(ivs)){
+for(j in 1:nrow(mt_fail_summary)){
   
+  # find decline scenarios that match the MT scenario in terms on nfail
+  mt <- mt_fail_summary[j, ]      # subset for one mt
+  ds <- l[[ mt$full_name ]][[1]]  # grab the decline scenario by name
+  ds <- 
+    mutate(ds, 
+         close_failn_low  = abs(failn_low  - mt$fail_low),
+         close_failn_high = abs(failn_high - mt$fail_high)) 
+  
+  k <- c(which.min(ds$close_failn_high), which.min(ds$close_failn_low))
+  k_seq <- sort((ds[k, ]$decline/10))
+  k_seq <- seq(k_seq[1], k_seq[2], 1)
+    
+  # consistency in names with 01_index.Rmd
+  ivs[[j]] <- rename(mt, low = fail_low, high = fail_high, 
+                     failp_low = fail_low_p, failp_high = fail_high_p) %>% 
+    mutate(cost_low  = min(ds[k,]$cost_low), 
+           cost_high = max(ds[k,]$cost_high))
+
   cv <- rep("gray60", ndeclines) # grey 
-  cv[ (seq(ivs$low[j], ivs$high[j], 10)/10) ] <- "black"
+  cv[ k_seq ] <- "black"
   
   # modeled results are out of order, so filter for the right gsa!
   # plotly 
-  p  <- ggplot(l[[  ivs$full_name[j]  ]]$failp, 
+  p  <- ggplot(l[[  mt$full_name  ]]$failp, 
                aes(x = decline, y = failp_mean, 
                    ymin = failp_low, ymax = failp_high)) +
-    geom_rect(aes(xmin = ivs$q1[j], xmax = ivs$q2[j], 
+    geom_rect(aes(xmin = 0, xmax = max(ds[k, ]$decline), 
                   ymin = 0, ymax = 1), fill = "red", alpha = 0.15) +      
     geom_point(aes(text=text), alpha = 0, color = cv) +
     geom_errorbar(color = cv) +
@@ -155,13 +184,14 @@ for(j in 1:nrow(ivs)){
            displaylogo = FALSE)
   col_vec <- rep("rgba(192,192,192,0.3)", ndeclines) # grey markers
   # red scenario marker
-  col_vec[ (seq(ivs$low[j], ivs$high[j], 10)/10) ] <- "rgba(255,0,0,0.3)"  
+  col_vec[ k_seq ] <- "rgba(255,0,0,0.3)"  
   p2$x$data[[2]]$marker$color <- col_vec             # modify plotly
   
-  write_rds(p2, here("plotly", paste0("mt_", ivs$gsp_name[j], ".rds")))
+  write_rds(p2, here("plotly", paste0("mt_", mt$gsp_name, ".rds")))
 }
 
-write_rds(ivs, here("code","results","ivs.rds"))
+bind_rows(ivs) %>% 
+  write_rds(here("code", "results", "mt_fail_summary_for_index.rds"))
 
 
 # ------------------------------------------------------------------------
@@ -169,8 +199,3 @@ write_rds(ivs, here("code","results","ivs.rds"))
 # ------------------------------------------------------------------------
 cat(paste(unique(gsa$gsp_name), collapse = "\n"))
 cat(paste(gsa_names, collapse = "\n"))
-
-# sanity check
-tibble(x=unique(gsa$gsp_name),y=gsa_names) %>% View()
-
-
